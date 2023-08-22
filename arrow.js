7d89f24a6d1485f92e78df8ae202e9b9bb12aa31
raselsharif <raselsharif9@gmail.com>
2023-08-23 00:40:43 +0600

    added arrow function

---
+++ arrow.js
@@ -0,0 +1,19 @@
+
+// ES-5 function way-01
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(10, 20));
+
+// ES-5 function way-02
+const add1 = function (a, b) {
+    return a + b;
+}
+console.log(add1(15, 25));
+
+// arrow function
+
+const add3 = (a,b) => a+b;
+console.log(add3(10,10));
+
